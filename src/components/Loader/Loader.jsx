bb40493d4d1f30c5da2ce7b090afcedcae38c78a
Tom Molton <moltontom@Toms-Air.home>
2024-02-23 19:33:42 +0000

    23/02/2024: Changed user info to localStorage and made league edits.

--- src/components/Loader/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import './loader.styles.css'
-
-export const Loader = () => {
-    return (
-        <div className="preloader">
-            <div className="loader"></div>
-        </div>
-    )
-}
