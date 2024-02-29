721cacdbeb71f221cc0f257a7c6f88de089623b9
Tom Molton <moltontom@Toms-Air.home>
2024-02-29 17:40:15 +0000

    Added prizes and appended points information

--- src/components/HowToPlay/HowToPlay.jsx
+++ src/components/HowToPlay/HowToPlay.jsx
@@ -14,8 +14,13 @@ export const HowToPlay = ({ backgroundColour, textColour, accentColour }) => {
                 <div className="sec1">
                     <h3>Overview:</h3>
                     <p>Gridlock is a F1 prediction app. Each weekend users predict which drivers they think will finish in what position.</p>
-                    <h3>Prize:</h3>
-                    <p>£100 cash.</p>
+                    <h3>Prizes:</h3>
+                    <p>The following prizes are for the final rankings in the global Gridlock standings.</p>
+                    <div className="prizes">
+                        <p className='prize'>2nd place: <span>£50</span></p>
+                        <p className='prize'>1st place: <span>£100</span></p>
+                        <p className='prize'>3rd place: <span>£25</span></p>
+                    </div>
                 </div>
                 <div className="sec2">
                     <h3>How to play:</h3>
@@ -35,17 +40,18 @@ export const HowToPlay = ({ backgroundColour, textColour, accentColour }) => {
                 <div className="sec4">
                     <h3>Points:</h3>
                     <div className="grid">
-                        <h1>1 point</h1>
+                        <h1>1 <span>point</span></h1>
                         <p>For predicting a driver to finish in the top 10.</p>
                     </div>
                     <div className="grid">
-                        <h1>3 points</h1>
+                        <h1>3 <span>points</span></h1>
                         <p>For predicting each driver in the correct position.</p>
                     </div>
                     <div className="grid">
-                        <h1>10 bonus points</h1>
+                        <h1>10 <span>bonus points</span></h1>
                         <p>For getting all 10 drivers in the correct position.</p>
                     </div>
+                    <p>The maximum available points per weekend is 40 points.</p>
                 </div>
             </div>
         </section>
