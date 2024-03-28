653a2108406a6357f1b23123be30f41c4353fd1f
Tom Molton <moltontom@Toms-Air.home>
2024-03-28 17:57:01 +0000

    edited calendar page further and previous predictions component

--- src/components/NextEventBox/NextEventCalendar.jsx
+++ src/components/NextEventBox/NextEventCalendar.jsx
@@ -10,29 +10,35 @@ import { DownChevronIcon, UpChevronIcon } from "../Icons/Icons"
 
 export const NextEventCalendar = ({ nextEvent, roundNumber, expanded }) => {
 
+    const [expandedItem, setExpandedItem] = useState(null);
+
+    const handleItemClick = (index) => {
+        setExpandedItem((prevExpandedItem) => (prevExpandedItem === index ? null : index));
+    };
+
+    const [resultData, setResultData] = useState(null);
+
     const fetchEventResult = async (event) => {
-        console.log(event)
         try {
             const response = await fetch('/api/externalData/CallApi.js', {
                 method: 'POST',
                 headers: {
                 'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(`https://v1.formula-1.api-sports.io/rankings/races?race=${event.id}`),
+                body: JSON.stringify(`/rankings/races?race=${event.id}`),
             });
           
             // Receive returned data and set state with data.
             if (response.ok) {
                   const responseData = await response.json();
-                  console.log(responseData);
-                //   const data = responseData.result.response;
-    
-                } else {
-                  console.log('failure');
-                }
-            } catch (error) {
-              console.error('Error submitting form:', error);
+                  const dataArray = responseData.result.response;
+                setResultData(dataArray);
+            } else {
+                console.log('failure');
             }
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
     }
 
     return (
@@ -60,7 +66,7 @@ export const NextEventCalendar = ({ nextEvent, roundNumber, expanded }) => {
             />
             <div className="competitions">
                 {nextEvent[0].events.slice().reverse().map((event, index) => (
-                    <div key={index} className="competition">
+                    <div key={index} className={`competition ${expandedItem === index ? 'expanded' : ''}`} onClick={(e) => { e.stopPropagation(); fetchEventResult(event); }}>
                         <div className="date">
                             <h3>{getCompetitionDate(event)}</h3>
                             <h4>{getCompetitionMonth(event)}</h4>
@@ -74,9 +80,14 @@ export const NextEventCalendar = ({ nextEvent, roundNumber, expanded }) => {
                             ) : (
                                 <>
                                     <h3>{event.type}</h3>
-                                    <div className="see-results" onClick={() => fetchEventResult(event)}>
+                                    <div className="see-results">
                                         <p>Results</p>
                                         <DownChevronIcon />
+                                        {resultData && resultData.map((result, index) => (
+                                            <div key={index}>
+                                                <p>{result.driver.name}</p>
+                                            </div>
+                                        ))}
                                     </div>
                                 </>
                             )}
--- src/components/PreviousPredictions/PreviousPredictions.jsx
+++ src/components/PreviousPredictions/PreviousPredictions.jsx
@@ -3,6 +3,7 @@ import { PrimaryHeading } from '../Typography/Titles/Titles';
 import { getCountryFlag } from '../../utils/getCountryFlag';
 import { ExpandIcon, CloseIcon, DownChevronIcon, UpChevronIcon } from '../Icons/Icons';
 import { LoaderWhite } from '../Loader/Loader';
+import { getTeamColour } from '../../utils/getTeamColour';
 import './previous-predictions.styles.css';
 
 export const PreviousPredictions = ({ seasonData, userEmail }) => {
@@ -92,27 +93,30 @@ export const PreviousPredictions = ({ seasonData, userEmail }) => {
                 previousEvents.map((event, index) => (
                     <div className={`event ${expandedEvents[index] && 'expanded'}`} key={index} onClick={() => handleClick(index, event.competitionId)}>
                         <div className="competition-bar">
-                            <div className="round">
-                                <h2>{index + 1}</h2>
+                            <div className='left'>
+                                <h3>R{index + 1}</h3>
+                                <figure className="circular-flag">
+                                    <img src={getCountryFlag(event.competitionCountry)} alt={`${event.competitionCountry} flag`} />
+                                </figure>
+                                <h3>{event.competitionCountry}</h3>
                             </div>
-                            <div className="name-and-flag" style={{ backgroundImage: `url(${getCountryFlag(event.competitionCountry)})` }}>
-                                <h2>{event.competitionCountry}</h2>
-                                <div className="opaque-layer"></div>
-                                {expandedEvents[index] ? <UpChevronIcon /> : <DownChevronIcon />}
-                            </div>
+                            {expandedEvents[index] ? <UpChevronIcon /> : <DownChevronIcon />}
                         </div>
                         {expandedEvents[index] && previousPrediction.length > 0 ? (
                                 <div className="prediction-details">
                                     {previousPrediction.map((prediction, index) => (
-                                        <div className="prediction" key={index}>
-                                            <div className="left">
-                                                <p className="position">P{index + 1}</p>
-                                                <div className="driver">
-                                                    <img src={prediction.driverImage} />
-                                                    <p>{prediction.driverFirstName}</p>
-                                                    <p className='name'>{prediction.driverLastName}</p>
+                                        <div key={index} className="driver-row">
+                                            <h3 className='position'>P{index + 1}</h3>
+                                                <div className='driver-details' style={{ border: `1px solid ${getTeamColour(prediction.driverTeam)}`, borderLeft: `5px solid ${getTeamColour(prediction.driverTeam)}` }}>
+                                                    <h6>{prediction.driverNumber}</h6>
+                                                    <div className="image">
+                                                        <img src={prediction.driverImage} alt={prediction.driverLastName} />
+                                                    </div>
+                                                    <div className="name">
+                                                        <p>{prediction.driverFirstName}</p>
+                                                        <h3>{prediction.driverLastName}</h3>
+                                                    </div>
                                                 </div>
-                                            </div>
                                         </div>
                                     ))}
                                     <div className="previous-points">
